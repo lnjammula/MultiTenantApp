1bc1bfdc264baaa6b40158bd5f6e26b25ec205cc
laxminarsimha.jammula@gmail.com <laxminarsimha.jammula@gmail.com>
2022-06-08 02:03:57 -0600

    prometheus with grafana exp and log customization

--- api/server.go
+++ api/server.go
@@ -1,7 +1,13 @@
 package api
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	db "multitenant.com/app/db/sqlc"
 )
 
@@ -14,12 +20,53 @@ type Server struct {
 //NewServer creates a new HTTP server and setup routing
 func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
-	router := gin.Default()
+
+	router := gin.New()
+
+	// define format for the log of routes
+	gin.DebugPrintRouteFunc = func(httpMethod, absolutePath, handlerName string, nuHandlers int) {
+		log.Printf("endpoint %v %v %v %v\n", httpMethod, absolutePath, handlerName, nuHandlers)
+	}
+
+	// LoggerWithFormatter middleware will write the logs to gin.DefaultWriter
+	// By default gin.DefaultWriter = os.Stdout
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		// your custom format
+		return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.TimeStamp.UTC(),
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+	router.Use(gin.Recovery())
 
 	//add routes to the router
 	router.POST("/users", server.createUser)
 	router.GET("/users/:id", server.getUser)
 	router.GET("/users", server.getUsers)
+
+	//prometheus
+	// Create a new registry.
+	reg := prometheus.NewRegistry()
+
+	// Add Go module build info.
+	reg.MustRegister(collectors.NewBuildInfoCollector())
+	reg.MustRegister(collectors.NewGoCollector(
+		collectors.WithGoCollections(collectors.GoRuntimeMemStatsCollection | collectors.GoRuntimeMetricsCollection),
+	))
+	router.GET("/metrics", gin.WrapH(promhttp.HandlerFor(
+		reg,
+		promhttp.HandlerOpts{
+			// Opt into OpenMetrics to support exemplars.
+			EnableOpenMetrics: true,
+		},
+	)))
 
 	server.router = router
 	return server
